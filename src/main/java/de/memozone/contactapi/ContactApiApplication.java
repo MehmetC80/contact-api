55d1497c18db269f3fa1d2e29cb488a1326b3ca9
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-12-10 12:09:02 +0100

    initial commit

---
+++ src/main/java/de/memozone/contactapi/ContactApiApplication.java
@@ -0,0 +1,13 @@
+package de.memozone.contactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/memozone/contactapi/ContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.memozone.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
